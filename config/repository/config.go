e216e93c2dbe18eac8c9161dded1a2119c353a19
James Dobson <dobson@softwarepunk.com>
2024-12-14 00:04:42 -0500

    🚨  Resolve lint warnings; should pass CI lint step now.

--- config/repository/config.go
+++ config/repository/config.go
@@ -1,7 +1,8 @@
-package artifactory_local_generic_repository
+package repository
 
 import "github.com/crossplane/upjet/pkg/config"
 
+// Configure the "artifactory_*_repository" resources.
 func Configure(p *config.Provider) {
 	p.AddResourceConfigurator("artifactory_local_generic_repository", func(r *config.Resource) {
 		r.ShortGroup = "repository"
